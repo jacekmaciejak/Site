2890c11ba43e22fffb91b080ee165a8e5f7cd34c
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-11-25 22:14:06 +0100

    Show & hide nav while scrolling

--- js/navbar.js
+++ js/navbar.js
@@ -16,3 +16,17 @@ function navbarLinkClick() {
     navbarToggler.click();
   }
 }
+
+// Show & hide nav while scrolling
+let lastScrollTop = 0;
+
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function() {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-80px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
